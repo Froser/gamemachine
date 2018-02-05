3d245e6123f0a49f8fd593376b24b3bf3692319b
Froser <hellofroser@hotmail.com>
2018-02-05 23:22:09 +0800

    * 突出单元测试结果

--- src/gamemachineunittest/unittestcase.cpp
+++ src/gamemachineunittest/unittestcase.cpp
@@ -2,13 +2,36 @@
 #include "unittestcase.h"
 #include <iomanip>
 
+namespace
+{
+#if GM_WINDOWS
+	void SetConsoleColor(WORD foreColor)
+	{
+		HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+		SetConsoleTextAttribute(handle, foreColor);
+	}
+#else
+	void SetConsoleColor(unsigned short)
+	{
+		// Nothing
+	}
+#endif
+}
+
 void assertTrue(bool ast, const std::string& msg, std::ostream& out = std::cout)
 {
+	::SetConsoleColor(FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
 	out << "Unit test: " << msg.c_str() << std::setw(15);
 	if (ast)
+	{
+		::SetConsoleColor(FOREGROUND_GREEN);
 		out << "Passed!" << std::endl;
+	}
 	else
+	{
+		::SetConsoleColor(FOREGROUND_RED);
 		out << "Failed!" << std::endl;
+	}
 }
 
 void UnitTest::addTestCase(const std::string& msg, TestPredicate p)
