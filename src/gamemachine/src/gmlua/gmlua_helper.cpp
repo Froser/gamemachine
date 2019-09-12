6377572c7fc2dcedfd4a98f7cfacb349faf62dce
Froser <hellofroser@hotmail.com>
2019-09-12 10:41:18 +0800

    增加了lua获取scalar的函数。 目前还没有实现获取function，也没有实现获取object。 需要在增加完上面那2总类型后，补充单元测试

--- src/gamemachine/src/gmlua/gmlua_helper.cpp
+++ src/gamemachine/src/gmlua/gmlua_helper.cpp
@@ -17,23 +17,6 @@ void GMReturnValues::pushArgument(const GMVariant& arg)
 }
 
 //////////////////////////////////////////////////////////////////////////
-GMint32 lua_Sizeof(GMMetaMemberType type)
-{
-	switch (type)
-	{
-	case GMMetaMemberType::Vector2:
-		return 2;
-	case GMMetaMemberType::Vector3:
-		return 3;
-	case GMMetaMemberType::Vector4:
-		return 4;
-	case GMMetaMemberType::Matrix4x4:
-		return 16;
-	default:
-		return 1;
-	}
-}
-
 GM_PRIVATE_OBJECT_UNALIGNED(GMLuaArguments)
 {
 	GMLuaCoreState* L;
@@ -41,12 +24,16 @@ GM_PRIVATE_OBJECT_UNALIGNED(GMLuaArguments)
 	GMint32 totalSize = 0;
 	Vector<GMint32> indices;
 
+	GMint32 typeSizeof(GMMetaMemberType type);
 	void calculateSize();
 	void pushVector(const GMVec2& v);
 	void pushVector(const GMVec3& v);
 	void pushVector(const GMVec4& v);
 	void pushMatrix(const GMMat4& v);
 	void push(const GMVariant& var);
+	GMVariant getScalar(GMint32 index);
+	GMint32 getIndexInStack(GMint32 offset, GMint32 index, OUT GMMetaMemberType* type = nullptr);
+	void checkType(const GMVariant& v, GMMetaMemberType mt, GMint32 index, const GMString& invoker);
 };
 
 template <typename T>
@@ -65,6 +52,23 @@ void __pushVector(const T& v, GMLuaCoreState* l)
 	}
 }
 
+GMint32 GMLuaArgumentsPrivate::typeSizeof(GMMetaMemberType type)
+{
+	switch (type)
+	{
+	case GMMetaMemberType::Vector2:
+		return 2;
+	case GMMetaMemberType::Vector3:
+		return 3;
+	case GMMetaMemberType::Vector4:
+		return 4;
+	case GMMetaMemberType::Matrix4x4:
+		return 16;
+	default:
+		return 1;
+	}
+}
+
 void GMLuaArgumentsPrivate::calculateSize()
 {
 	totalSize = 0;
@@ -72,7 +76,7 @@ void GMLuaArgumentsPrivate::calculateSize()
 	GMint32 idx = 0;
 	for (auto& type : types)
 	{
-		totalSize += lua_Sizeof(type);
+		totalSize += typeSizeof(type);
 		indices[idx++] = totalSize;
 	}
 }
@@ -162,6 +166,79 @@ void GMLuaArgumentsPrivate::pushMatrix(const GMMat4& v)
 	}
 }
 
+GMVariant GMLuaArgumentsPrivate::getScalar(GMint32 index)
+{
+	if (lua_isinteger(L, index))
+		return static_cast<GMint32>(lua_tointeger(L, index));
+	if (lua_isnumber(L, index))
+		return lua_tonumber(L, index);
+	if (lua_isstring(L, index))
+		return GMString(lua_tostring(L, index));
+	if (lua_isboolean(L, index))
+		return lua_toboolean(L, index) ? true : false;
+	gm_error(gm_dbg_wrap("GMLua (pop): type not supported"));
+	return GMVariant();
+}
+
+GMint32 GMLuaArgumentsPrivate::getIndexInStack(GMint32 offset, GMint32 index, OUT GMMetaMemberType* t)
+{
+	GMint32 result = 0;
+	GMint32 i = 0;
+	for (auto type : types)
+	{
+		result += typeSizeof(type);
+		if (++i >= index)
+		{
+			if (t)
+				*t = type;
+			break;
+		}
+	}
+	return offset + result;
+}
+
+void GMLuaArgumentsPrivate::checkType(const GMVariant& v, GMMetaMemberType mt, GMint32 index, const GMString& invoker)
+{
+	bool suc = false;
+	switch (mt)
+	{
+	case GMMetaMemberType::Int:
+	case GMMetaMemberType::Float:
+		suc = v.isFloat() || v.isInt() || v.isInt64();
+		break;
+	case GMMetaMemberType::Vector2:
+		suc = v.isVec2();
+		break;
+	case GMMetaMemberType::Vector3:
+		suc = v.isVec3();
+		break;
+	case GMMetaMemberType::Vector4:
+		suc = v.isVec4();
+		break;
+	case GMMetaMemberType::Matrix4x4:
+		suc = v.isMat4();
+		break;
+	case GMMetaMemberType::String:
+		suc = v.isString();
+		break;
+	case GMMetaMemberType::Boolean:
+		suc = v.isBool();
+		break;
+	case GMMetaMemberType::Object:
+	case GMMetaMemberType::Pointer:
+	case GMMetaMemberType::Function:
+	case GMMetaMemberType::Invalid:
+	default:
+		break;
+	}
+
+	std::string ivk = invoker.toStdString();
+	if (!suc)
+	{
+		luaL_error(L, "Type is not match at index %i in %s", index, ivk.c_str());
+	}
+}
+
 GMLuaArguments::GMLuaArguments(GMLuaCoreState* l, std::initializer_list<GMMetaMemberType> types)
 {
 	GM_CREATE_DATA();
@@ -184,8 +261,20 @@ bool GMLuaArguments::isValid()
 
 GMVariant GMLuaArguments::getArgument(GMint32 index, const GMString& invoker)
 {
+	D(d);
 	// 先从index中找到索引
-	return GMVariant();
+	GMMetaMemberType type = GMMetaMemberType::Invalid;
+	GMint32 i = d->getIndexInStack(0, index, &type);
+	
+	GMVariant v;
+	if (type != GMMetaMemberType::Object)
+		v = d->getScalar(i);
+	else
+		// v = d->getObject(i);
+
+	// 检查这个类型是否与期望的一致
+	d->checkType(v, type, index, invoker);
+	return v;
 }
 
 void GMLuaArguments::pushArgument(const GMVariant& arg)
